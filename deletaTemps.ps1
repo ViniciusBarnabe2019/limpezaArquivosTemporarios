7b8e27ed0c6bd578833df7d3c6a5085ac3fcc8d8
ViniciusBarnabe2019 <>
2023-02-19 17:32:33 -0300

    Update to Admin Privilege

--- deletaTemps.ps1
+++ deletaTemps.ps1
@@ -1,11 +1,28 @@
-#Deleta Arquivos da Pasta %TEMP%
-Remove-Item -Path "$env:TEMP\*" -Force -recurse -ErrorAction SilentlyContinue;
+# Função que deleta arquivos
+function deletaArquivos {
+	param($pasta)
+    if($pasta -eq "temp1") {
+        Remove-Item -Path "$env:TEMP\*" -Force -Recurse -ErrorAction SilentlyContinue
+    }
+    elseif($pasta -eq "temp2") {
+        Remove-Item -Path "C:\Windows\Temp\*" -Force -Recurse -ErrorAction SilentlyContinue
+    }
+    elseif($pasta -eq "pref") {
+        Remove-Item -Path "C:\Windows\Prefetch\*" -Force -Recurse -ErrorAction SilentlyContinue
+    }
+    elseif($pasta -eq "lixeira") {
+        Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+    }
+}
 
-#Deleta Arquivos da Pasta TEMP
-Remove-Item -Path "C:\Windows\Temp\*" -Force -recurse -ErrorAction SilentlyContinue;
+# Deleta arquivos da pasta Temp1
+deletaArquivos -pasta "temp1"
 
-#Deleta Arquivos da Pasta PREFETCH
-Remove-Item -Path "C:\Windows\Prefetch\*" -Force -recurse -ErrorAction SilentlyContinue;
+# Deleta arquivos da pasta Temp2
+deletaArquivos -pasta "temp2"
 
-#Esvazia a Lixeira
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue;
+# Esvazia a Lixeira
+deletaArquivos -pasta "lixeira"
+
+# Deleta arquivos da pasta Prefetch
+deletaArquivos -pasta "pref"
